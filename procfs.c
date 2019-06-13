8a7abbcbe9f9b3f330c086079ee44be8181142d3
Tal <tal.lavon1@gmail.com>
2019-06-13 18:15:37 +0300

    error fix

--- procfs.c
+++ procfs.c
@@ -161,7 +161,7 @@ update_inode_entries(int inum)
   int j = 2;
   for (int i = 0; i < NINODE; i++) {
     if (inode_entries[i].used) {
-      int index = get_indoe_index(inode_entries[i].inode->inum);
+      int index = get_inode_index(inode_entries[i].inode->inum);
       itoa(inodeinfo_dir_entries[j].name, index);
       inodeinfo_dir_entries[j].inum = 900 + index;
       j++;
